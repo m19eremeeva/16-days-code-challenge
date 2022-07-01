1cee53a6b2d7c5b701e4b406c1c44666220417e9
Maria Eremeeva <m19eremeeva@yandex.ru>
2022-07-01 23:25:51 +0300

    Import clear function

--- Day07_Hangman_game/Day07_Hangman game.py
+++ Day07_Hangman_game/Day07_Hangman game.py
@@ -1,9 +1,7 @@
 import random
-
+from replit import clear
 import hangman_art
 import hangman_words
-
-
 
 word_list = hangman_words.word_list
 chosen_word = random.choice(word_list)
@@ -18,22 +16,24 @@ for i in range(len(chosen_word)):
 print(hangman_art.logo)
 while not end_of_game:
     guess = input('Guess the letter: ').lower()
+    clear()
     if guess in display:
         print(f'You already guessed {guess}')
     for i in range(len(chosen_word)):
         if guess == chosen_word[i]:
             display[i] = chosen_word[i]
+
+
+    print(f"{' '.join(display)}")
+    print(hangman_art.stages[lives])
+    if "_" not in display:
+        end_of_game = True
+        print(hangman_art.stages[lives])
+        print("You win.")
     if guess not in chosen_word:
         lives -= 1
         print(f'You guessed {guess}. That is not in the word. You lose a life.')
         if lives == 0:
             end_of_game = True
+            print(hangman_art.stages[lives])
             print("You lose.")
-
-    print(f"{' '.join(display)}")
-
-    if "_" not in display:
-        end_of_game = True
-        print("You win.")
-
-    print(hangman_art.stages[lives])
